83139dd31603b29a95459040f3cc065f4d9be089
Kevin Sorensen <kevin@fdtr.ca>
2016-09-13 21:00:26 -0600

    added POST /todos

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 var PORT = process.env.PORT || 3000;
-var todos = [
-  {
-    id: 1,
-    description: "resign H&R",
-    completed: false
-  },{
-    id: 2,
-    description: "pet cat",
-    completed: false
-  },{
-    id: 3,
-    description: "go to bed",
-    completed: true
-  }
-];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 // GET /todos
 app.get('/todos', function(req, res) {
   res.json(todos);
 });
+
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id,10);
@@ -32,13 +24,22 @@ app.get('/todos/:id', function(req, res) {
   } else {
     res.json(retTodo);
   }
+});
 
-  //res.send('Request for todo id: = ' + req.params.id);
+// POST /todos/:id
+app.post('/todos', function(req, res) {
+  var body = req.body;
+
+  body.id = todoNextId++;
+  todos.push(body);
+
+  res.json(body);
 });
 
 app.get('/', function(req, res) {
   res.send('Todo API Root');
 });
+
 
 app.listen(PORT, function () {
  console.log('Express listening on port ' + PORT + '!');
