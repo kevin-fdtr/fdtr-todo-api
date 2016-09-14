7c8cbeb7cca88bfb09863daec8c4b10c7264894d
Kevin Sorensen <kevin@fdtr.ca>
2016-09-13 22:16:40 -0600

    added DELETE /todos/:id

--- server.js
+++ server.js
@@ -38,11 +38,27 @@ app.post('/todos', function(req, res) {
     return res.status(400).send();
   }
   body.description = body.description.trim();
-  
+
   body.id = todoNextId++;
   todos.push(body);
   res.json(body);
 });
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req,res) {
+  var deleteId = parseInt(req.params.id,10);
+
+  var deleteTodo = _.findWhere(todos, {id: deleteId});
+  todos = _.without(todos, deleteTodo);
+
+  if (typeof deleteTodo === 'undefined') {
+    res.status(404).send();
+  }
+  else {
+    res.json(deleteTodo);
+  }
+});
+
 
 app.get('/', function(req, res) {
   res.send('Todo API Root');
