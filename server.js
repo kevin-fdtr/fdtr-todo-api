919543b75e9e942ab6e2913f8b72ac5496c3641d
Kevin Sorensen <kevin@fdtr.ca>
2016-09-13 21:41:23 -0600

    added underscore and validation of post todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -16,9 +17,12 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id,10);
-  var retTodo = todos.find(function(todo) {
-    return todo.id === todoId;
-  });
+
+  var retTodo = _.findWhere(todos, {id: todoId});
+
+  // var retTodo = todos.find(function(todo) {
+  //   return todo.id === todoId;
+  // });
   if (typeof retTodo === 'undefined') {
     res.status(404).send();
   } else {
@@ -28,11 +32,15 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos/:id
 app.post('/todos', function(req, res) {
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed' );
 
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+  body.description = body.description.trim();
+  
   body.id = todoNextId++;
   todos.push(body);
-
   res.json(body);
 });
 
