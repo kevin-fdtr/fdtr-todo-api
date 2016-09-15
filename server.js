e145b8c2f1ffa77853efb4026ea606aad9897008
Kevin Sorensen <kevin@fdtr.ca>
2016-09-14 21:58:11 -0600

    added /todos?q support

--- server.js
+++ server.js
@@ -9,9 +9,29 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-// GET /todos
+//GET /
+app.get('/', function(req, res) {
+  req.send('Todo API root');
+});
+
+//GET /todos?completed=
 app.get('/todos', function(req, res) {
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+
+  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+    filteredTodos = _.where(filteredTodos, {completed: true});
+  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    filteredTodos = _.where(filteredTodos, {completed: false});
+  }
+
+  if (queryParams.hasOwnProperty('q') && queryParams.q.trim().length > 0) {
+    filteredTodos = _.filter(filteredTodos, function(todo) {
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    });
+  }
+
+  res.json(filteredTodos);
 });
 
 // GET /todos/:id
