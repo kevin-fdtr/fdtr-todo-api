f4d8cb984e3b61a833ed03cc23c688b48a4a00c0
Kevin Sorensen <kevin@fdtr.ca>
2016-09-15 19:48:57 -0600

    wired up sqlite

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -47,18 +48,26 @@ app.get('/todos/:id', function(req, res) {
   }
 });
 
-// POST /todos/:id
+// POST /todos
 app.post('/todos', function(req, res) {
   var body = _.pick(req.body, 'description', 'completed' );
 
-  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    return res.status(400).send();
-  }
-  body.description = body.description.trim();
+  // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+  //   return res.status(400).send();
+  // }
+  // body.description = body.description.trim();
+  //
+  // body.id = todoNextId++;
 
-  body.id = todoNextId++;
-  todos.push(body);
-  res.json(body);
+  db.todo.create(body)
+  .then( function(todo) {
+    res.json(todo.toJSON());
+  }, function(e) {
+    res.status(400).json(e);
+  });
+
+//  todos.push(body);
+//  res.json(body);
 });
 
 // DELETE /todos/:id
@@ -108,7 +117,8 @@ app.get('/', function(req, res) {
   res.send('Todo API Root');
 });
 
-
-app.listen(PORT, function () {
- console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then( function() {
+  app.listen(PORT, function () {
+   console.log('Express listening on port ' + PORT + '!');
+  });
 });
