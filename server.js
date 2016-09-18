c52f9d28fa7496ccae38c8c055b940ef21b7a5c6
Kevin Sorensen <kevin@fdtr.ca>
2016-09-18 15:59:58 -0600

    added DELETE users/login

--- server.js
+++ server.js
@@ -42,7 +42,6 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
   db.todo.findAll(where).then( function(todos) {
     res.json(todos);
   }, function(e) {
-    console.log(e);
     res.status(500).send();
   });
 });
@@ -116,11 +115,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req,res) {
 
 // PUT /todos/:id
 app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
-console.log('req.params');
-console.log(req.params);
-
 var updateId = parseInt(req.params.id,10);
-console.log(updateId);
+
   if (isNaN(updateId)) {
     return res.status(400).send();
   }
@@ -176,16 +172,33 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
   var body = _.pick(req.body, 'email', 'password');
+  var userInstance;
 
   db.user.authenticate(body).then( function(user) {
     var token = user.generateToken('authentication');
-    if (token) {
-      res.header('Auth', token).json(user.toPublicJSON());
-    } else {
-      res.status(401).send();
-    }
-  }, function() {
+    userInstance = user;
+    return db.token.create({
+      token: token
+    });
+
+    // if (token) {
+    //   res.header('Auth', token).json(user.toPublicJSON());
+    // } else {
+    //   res.status(401).send();
+    // }
+  }).then( function(tokenInstance) {
+    res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+  }).catch( function() {
     res.status(401).send();
+  });
+});
+
+// POST /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+  req.token.destroy().then( function () {
+    res.status(204).send();
+  }).catch(function () {
+    res.status(500).send();
   });
 });
 
@@ -193,6 +206,6 @@ db.sequelize.sync(
   {force:true}
 ).then( function() {
   app.listen(PORT, function () {
-   console.log('Express listening on port ' + PORT + '!!');
+   console.log('Express listening on port ' + PORT + '!');
   });
 });
